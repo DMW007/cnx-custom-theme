5c913a8b9ae93c2119afd5a059bb3b909fcd8f34
DMW007 <21245043+DMW007@users.noreply.github.com>
2021-02-03 14:56:58 +0100

    Added dedicate scss file for touchpoint to apply the look & feel to apps with different UI

--- gulpfile.js
+++ gulpfile.js
@@ -37,6 +37,11 @@ let outputFiles = {
     fullBundle: 'custom-all.css',
     indexInputFile: `${srcFolder}/scss/index.scss`
   }, 
+  cssTouchpoint: {
+    baseDir: `${outputFolder}/css`,
+    fullBundle: 'custom-touchpoint.css',
+    indexInputFile: `${srcFolder}/scss/dedicated-bundles/touchpoint/touchpoint.scss`
+  }, 
   js: {
     baseDir: `${outputFolder}/js`,
     custom: 'custom.js'
@@ -47,6 +52,24 @@ let cleanCssCallback = details => {
   console.log(`\t${details.name}:  ${details.stats.minifiedSize / 1000}/${details.stats.originalSize / 1000} => ${saved / 1000} KB gespart`
   );
 };
+
+// We seperate some apps cause they were not properly integrated into CNX from a UI perspective. So it doesn't make sense to include our CNX bundle here,
+// it doesn't match and would only cause a lot of overhead/potential for conflicts. But we can still use our color-vars in them, to give them the same
+// look and feel like we applied for connections without redundance.
+gulp.task("scss-touchpoint", () => {
+  return gulp
+  .src(outputFiles.cssTouchpoint.indexInputFile)
+  .pipe(wait(200))
+  .pipe(productionBuild ? noop() : sourcemaps.init())
+  .pipe(sass())
+  .pipe(rename(outputFiles.cssTouchpoint.fullBundle))
+  .pipe(
+    productionBuild ? cleanCSS(cleanCssOptions, cleanCssCallback) : noop()
+  )
+  .pipe(productionBuild ? sourcemaps.write() : noop())
+  .pipe(header('/`This file was automatically generated using gulp. Please apply changes to the original scss sources files. */\n'))
+  .pipe(gulp.dest(outputFiles.cssTouchpoint.baseDir));
+})
 
 // Divide own CSS and included libraries for faster live-reloading (when usually just our own css changes)
 gulp.task("scss", () => {
@@ -116,7 +139,7 @@ gulp.task("bundle-all", gulp.series("scss", "css-lib", () => {
     .pipe(replace(" addWithoutChange */", ""))
     .pipe(gulp.dest(outputFiles.css.baseDir));
 }));
-gulp.task("css", gulp.series("scss", "css-lib", "bundle-all"));
+gulp.task("css", gulp.series("scss", "scss-touchpoint", "css-lib", "bundle-all"));
 
 gulp.task("watch-css", () => {
   var watcher = gulp.watch("src/**/*.scss", gulp.series("scss", "bundle-all"));
